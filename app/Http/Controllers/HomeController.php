ea30d868afd9ba2659c07553e92f776a08d09757
unknown <miroslav.nedelchev@dentacoin.com>
2019-06-27 09:53:13 +0300

    fix file_get_contents and replace it with curl

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,7 +15,7 @@ class HomeController extends Controller
     protected function getView()   {
         setlocale (LC_ALL, 'de_DE');
         //$latest_blog_articles = DB::connection('mysql3')->select(DB::raw("SELECT `post_title`, `post_name` from dIf_posts WHERE post_status = 'publish' AND post_type = 'post' ORDER BY `post_date` DESC LIMIT 0, 5"));
-        $latest_blog_articles = json_decode(file_get_contents('https://blog.dentacoin.com/dumb-latest-posts/'));
+
         $testimonials = DB::connection('mysql2')->table('user_expressions')->leftJoin('media', 'user_expressions.media_id', '=', 'media.id')->select('user_expressions.*', 'media.name as media_name', 'media.alt as media_alt')->orderByRaw('user_expressions.order_id ASC')->get()->toArray();
         $applications = DB::connection('mysql2')->table('applications')->select('applications.*')->orderByRaw('applications.order_id ASC')->get()->toArray();
         foreach($applications as $application) {
@@ -27,7 +27,23 @@ class HomeController extends Controller
             $application->popup_logo_url = DB::connection('mysql2')->table('media')->where('id', $application->popup_logo_id)->select('media.name')->get()->first()->name;
         }
 
-        return view("pages/homepage", ['testimonials' => $testimonials, 'team_members' => (new TeamMembersController())->getAllTeamMembers(), 'slider' => (new HomepageSlidesController())->getAllHomepageSlides(), 'highlights' => (new HighlightsController())->getAllHighlights(), 'gallery' => (new HomepageGalleryPhotosController())->getAllHomepageGalleryPhotos(), 'applications' => $applications, 'latest_blog_articles' => $latest_blog_articles]);
+        $params = ['testimonials' => $testimonials, 'team_members' => (new TeamMembersController())->getAllTeamMembers(), 'slider' => (new HomepageSlidesController())->getAllHomepageSlides(), 'highlights' => (new HighlightsController())->getAllHighlights(), 'gallery' => (new HomepageGalleryPhotosController())->getAllHomepageGalleryPhotos(), 'applications' => $applications];
+
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL => 'https://blog.dentacoin.com/dumb-latest-posts/',
+            CURLOPT_SSL_VERIFYPEER => 0
+        ));
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        $resp = json_decode(curl_exec($curl));
+        curl_close($curl);
+
+        if(!empty($resp))   {
+            $params['latest_blog_articles'] = $resp;
+        }
+
+        return view('pages/homepage', $params);
     }
 }
 
